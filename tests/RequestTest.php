9b693a77fb2d9f336bf460fcd7899319a584fcaf
Natan Felles <natanfelles@gmail.com>
2021-06-26 10:36:17 -0300

    Update Coding Standard - header comment and spaces

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\HTTP\Client;
+<?php
+/*
+ * This file is part of The Framework HTTP Client Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\HTTP\Client;
 
 use Framework\HTTP\Client\Request;
 use Framework\HTTP\Cookie;
