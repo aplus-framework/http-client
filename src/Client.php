17c2bca157910a1073a1311e9b287e505f911fab
Natan Felles <natanfelles@gmail.com>
2021-07-20 16:15:45 -0300

    Allow strict check cURL options

--- src/Client.php
+++ src/Client.php
@@ -13,6 +13,7 @@ use CURLFile;
 use Framework\Helpers\ArraySimple;
 use InvalidArgumentException;
 use JetBrains\PhpStorm\Pure;
+use OutOfBoundsException;
 use RuntimeException;
 
 /**
@@ -76,11 +77,12 @@ class Client
      * @var array<string,mixed>
      */
     protected array $info = [];
+    protected bool $checkOptions = false;
 
     /**
      * Set cURL options.
      *
-     * @param int $option A cURL CURLOPT_* constant
+     * @param int $option A cURL constant
      * @param mixed $value
      *
      * @see Client::$defaultOptions
@@ -89,8 +91,263 @@ class Client
      */
     public function setOption(int $option, mixed $value) : static
     {
+        if ($this->isCheckingOptions()) {
+            $this->checkOption($option, $value);
+        }
         $this->options[$option] = $value;
         return $this;
+    }
+
+    /**
+     * @param bool $check
+     *
+     * @return static
+     */
+    public function setCheckOptions(bool $check = true) : static
+    {
+        $this->checkOptions = $check;
+        return $this;
+    }
+
+    public function isCheckingOptions() : bool
+    {
+        return $this->checkOptions;
+    }
+
+    /**
+     * @param int $option The cURL option
+     * @param mixed $value The cURL option value
+     *
+     * @throws InvalidArgumentException if the option value does not match the
+     * expected type
+     * @throws OutOfBoundsException if the option is invalid
+     */
+    protected function checkOption(int $option, mixed $value) : void
+    {
+        $types = [
+            'bool' => [
+                \CURLOPT_AUTOREFERER,
+                \CURLOPT_COOKIESESSION,
+                \CURLOPT_CERTINFO,
+                \CURLOPT_CONNECT_ONLY,
+                \CURLOPT_CRLF,
+                \CURLOPT_DISALLOW_USERNAME_IN_URL,
+                \CURLOPT_DNS_SHUFFLE_ADDRESSES,
+                \CURLOPT_HAPROXYPROTOCOL,
+                \CURLOPT_SSH_COMPRESSION,
+                \CURLOPT_DNS_USE_GLOBAL_CACHE,
+                \CURLOPT_FAILONERROR,
+                \CURLOPT_SSL_FALSESTART,
+                \CURLOPT_FILETIME,
+                \CURLOPT_FOLLOWLOCATION,
+                \CURLOPT_FORBID_REUSE,
+                \CURLOPT_FRESH_CONNECT,
+                \CURLOPT_FTP_USE_EPRT,
+                \CURLOPT_FTP_USE_EPSV,
+                \CURLOPT_FTP_CREATE_MISSING_DIRS,
+                \CURLOPT_FTPAPPEND,
+                \CURLOPT_TCP_NODELAY,
+                \CURLOPT_FTPASCII,
+                \CURLOPT_FTPLISTONLY,
+                \CURLOPT_HEADER,
+                \CURLINFO_HEADER_OUT,
+                \CURLOPT_HTTP09_ALLOWED,
+                \CURLOPT_HTTPGET,
+                \CURLOPT_HTTPPROXYTUNNEL,
+                \CURLOPT_HTTP_CONTENT_DECODING,
+                \CURLOPT_KEEP_SENDING_ON_ERROR,
+                \CURLOPT_MUTE,
+                \CURLOPT_NETRC,
+                \CURLOPT_NOBODY,
+                \CURLOPT_NOPROGRESS,
+                \CURLOPT_NOSIGNAL,
+                \CURLOPT_PATH_AS_IS,
+                \CURLOPT_PIPEWAIT,
+                \CURLOPT_POST,
+                \CURLOPT_PUT,
+                \CURLOPT_RETURNTRANSFER,
+                \CURLOPT_SASL_IR,
+                \CURLOPT_SSL_ENABLE_ALPN,
+                \CURLOPT_SSL_ENABLE_NPN,
+                \CURLOPT_SSL_VERIFYPEER,
+                \CURLOPT_SSL_VERIFYSTATUS,
+                \CURLOPT_PROXY_SSL_VERIFYPEER,
+                \CURLOPT_SUPPRESS_CONNECT_HEADERS,
+                \CURLOPT_TCP_FASTOPEN,
+                \CURLOPT_TFTP_NO_OPTIONS,
+                \CURLOPT_TRANSFERTEXT,
+                \CURLOPT_UNRESTRICTED_AUTH,
+                \CURLOPT_UPLOAD,
+                \CURLOPT_VERBOSE,
+            ],
+            'int' => [
+                \CURLOPT_BUFFERSIZE,
+                \CURLOPT_CONNECTTIMEOUT,
+                \CURLOPT_CONNECTTIMEOUT_MS,
+                \CURLOPT_DNS_CACHE_TIMEOUT,
+                \CURLOPT_EXPECT_100_TIMEOUT_MS,
+                \CURLOPT_HAPPY_EYEBALLS_TIMEOUT_MS,
+                \CURLOPT_FTPSSLAUTH,
+                \CURLOPT_HEADEROPT,
+                \CURLOPT_HTTP_VERSION,
+                \CURLOPT_HTTPAUTH,
+                \CURLOPT_INFILESIZE,
+                \CURLOPT_LOW_SPEED_LIMIT,
+                \CURLOPT_LOW_SPEED_TIME,
+                \CURLOPT_MAXCONNECTS,
+                \CURLOPT_MAXREDIRS,
+                \CURLOPT_PORT,
+                \CURLOPT_POSTREDIR,
+                \CURLOPT_PROTOCOLS,
+                \CURLOPT_PROXYAUTH,
+                \CURLOPT_PROXYPORT,
+                \CURLOPT_PROXYTYPE,
+                \CURLOPT_REDIR_PROTOCOLS,
+                \CURLOPT_RESUME_FROM,
+                \CURLOPT_SOCKS5_AUTH,
+                \CURLOPT_SSL_OPTIONS,
+                \CURLOPT_SSL_VERIFYHOST,
+                \CURLOPT_SSLVERSION,
+                \CURLOPT_PROXY_SSL_OPTIONS,
+                \CURLOPT_PROXY_SSL_VERIFYHOST,
+                \CURLOPT_PROXY_SSLVERSION,
+                \CURLOPT_STREAM_WEIGHT,
+                \CURLOPT_TCP_KEEPALIVE,
+                \CURLOPT_TCP_KEEPIDLE,
+                \CURLOPT_TCP_KEEPINTVL,
+                \CURLOPT_TIMECONDITION,
+                \CURLOPT_TIMEOUT,
+                \CURLOPT_TIMEOUT_MS,
+                \CURLOPT_TIMEVALUE,
+                \CURLOPT_TIMEVALUE_LARGE,
+                \CURLOPT_MAX_RECV_SPEED_LARGE,
+                \CURLOPT_MAX_SEND_SPEED_LARGE,
+                \CURLOPT_SSH_AUTH_TYPES,
+                \CURLOPT_IPRESOLVE,
+                \CURLOPT_FTP_FILEMETHOD,
+            ],
+            'string' => [
+                \CURLOPT_ABSTRACT_UNIX_SOCKET,
+                \CURLOPT_CAINFO,
+                \CURLOPT_CAPATH,
+                \CURLOPT_COOKIE,
+                \CURLOPT_COOKIEFILE,
+                \CURLOPT_COOKIEJAR,
+                \CURLOPT_COOKIELIST,
+                \CURLOPT_CUSTOMREQUEST,
+                \CURLOPT_DEFAULT_PROTOCOL,
+                \CURLOPT_DNS_INTERFACE,
+                \CURLOPT_DNS_LOCAL_IP4,
+                \CURLOPT_DNS_LOCAL_IP6,
+                \CURLOPT_EGDSOCKET,
+                \CURLOPT_ENCODING,
+                \CURLOPT_FTPPORT,
+                \CURLOPT_INTERFACE,
+                \CURLOPT_KEYPASSWD,
+                \CURLOPT_KRB4LEVEL,
+                \CURLOPT_LOGIN_OPTIONS,
+                \CURLOPT_PINNEDPUBLICKEY,
+                \CURLOPT_POSTFIELDS,
+                \CURLOPT_PRIVATE,
+                \CURLOPT_PRE_PROXY,
+                \CURLOPT_PROXY,
+                \CURLOPT_PROXY_SERVICE_NAME,
+                \CURLOPT_PROXY_CAINFO,
+                \CURLOPT_PROXY_CAPATH,
+                \CURLOPT_PROXY_CRLFILE,
+                \CURLOPT_PROXY_KEYPASSWD,
+                \CURLOPT_PROXY_PINNEDPUBLICKEY,
+                \CURLOPT_PROXY_SSLCERT,
+                \CURLOPT_PROXY_SSLCERTTYPE,
+                \CURLOPT_PROXY_SSL_CIPHER_LIST,
+                \CURLOPT_PROXY_TLS13_CIPHERS,
+                \CURLOPT_PROXY_SSLKEY,
+                \CURLOPT_PROXY_SSLKEYTYPE,
+                \CURLOPT_PROXY_TLSAUTH_PASSWORD,
+                \CURLOPT_PROXY_TLSAUTH_TYPE,
+                \CURLOPT_PROXY_TLSAUTH_USERNAME,
+                \CURLOPT_PROXYUSERPWD,
+                \CURLOPT_RANDOM_FILE,
+                \CURLOPT_RANGE,
+                \CURLOPT_REFERER,
+                \CURLOPT_SERVICE_NAME,
+                \CURLOPT_SSH_HOST_PUBLIC_KEY_MD5,
+                \CURLOPT_SSH_PUBLIC_KEYFILE,
+                \CURLOPT_SSH_PRIVATE_KEYFILE,
+                \CURLOPT_SSL_CIPHER_LIST,
+                \CURLOPT_SSLCERT,
+                \CURLOPT_SSLCERTPASSWD,
+                \CURLOPT_SSLCERTTYPE,
+                \CURLOPT_SSLENGINE,
+                \CURLOPT_SSLENGINE_DEFAULT,
+                \CURLOPT_SSLKEY,
+                \CURLOPT_SSLKEYPASSWD,
+                \CURLOPT_SSLKEYTYPE,
+                \CURLOPT_TLS13_CIPHERS,
+                \CURLOPT_UNIX_SOCKET_PATH,
+                \CURLOPT_URL,
+                \CURLOPT_USERAGENT,
+                \CURLOPT_USERNAME,
+                \CURLOPT_PASSWORD,
+                \CURLOPT_USERPWD,
+                \CURLOPT_XOAUTH2_BEARER,
+            ],
+            'array' => [
+                \CURLOPT_CONNECT_TO,
+                \CURLOPT_HTTP200ALIASES,
+                \CURLOPT_HTTPHEADER,
+                \CURLOPT_POSTQUOTE,
+                \CURLOPT_PROXYHEADER,
+                \CURLOPT_QUOTE,
+                \CURLOPT_RESOLVE,
+            ],
+            'fopen' => [
+                \CURLOPT_FILE,
+                \CURLOPT_INFILE,
+                \CURLOPT_STDERR,
+                \CURLOPT_WRITEHEADER,
+            ],
+            'function' => [
+                \CURLOPT_HEADERFUNCTION,
+                \CURLOPT_PASSWDFUNCTION,
+                \CURLOPT_PROGRESSFUNCTION,
+                \CURLOPT_READFUNCTION,
+                \CURLOPT_WRITEFUNCTION,
+            ],
+            'curl_share_init' => [
+                \CURLOPT_SHARE,
+            ],
+        ];
+        foreach ($types as $type => $constants) {
+            foreach ($constants as $constant) {
+                if ($option !== $constant) {
+                    continue;
+                }
+                $valid = match ($type) {
+                    'bool' => \is_bool($value),
+                    'int' => \is_int($value),
+                    'string' => \is_string($value),
+                    'array' => \is_array($value),
+                    'fopen' => \is_resource($value),
+                    'function' => \is_callable($value),
+                    'curl_share_init' => $value instanceof \CurlShareHandle
+                };
+                if ($valid) {
+                    return;
+                }
+                $message = match ($type) {
+                    'bool' => 'The value of option %d should be of bool type',
+                    'int' => 'The value of option %d should be of int type',
+                    'string' => 'The value of option %d should be of string type',
+                    'array' => 'The value of option %d should be of array type',
+                    'fopen' => 'The value of option %d should be a fopen() resource',
+                    'function' => 'The value of option %d should be a callable',
+                    'curl_share_init' => 'The value of option %d should be a result of curl_share_init()'
+                };
+                throw new InvalidArgumentException(\sprintf($message, $option));
+            }
+        }
+        throw new OutOfBoundsException('Invalid cURL constant option: ' . $option);
     }
 
     /**
