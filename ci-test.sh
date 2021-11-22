458b79dbae394efac1df24e870471066bd30511b
Natan Felles <natanfelles@gmail.com>
2021-11-22 18:51:40 -0300

    Disable PHP-CS-Fixer in CI files

--- ci-test.sh
+++ ci-test.sh
@@ -4,7 +4,7 @@ set -e
 commands=(
     "composer install"
     "composer normalize --dry-run --indent-size=4 --indent-style=space"
-    "vendor/bin/php-cs-fixer fix --diff --dry-run --verbose"
+    # "vendor/bin/php-cs-fixer fix --diff --dry-run --verbose"
     "vendor/bin/phpmd src xml phpmd.xml"
     "vendor/bin/phpstan analyse -vvv"
     "vendor/bin/phpunit"
