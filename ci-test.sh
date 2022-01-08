21749f0ce0160405c4869806cdde5b68ef960eb3
Natan Felles <natanfelles@gmail.com>
2022-01-08 03:49:32 -0300

    Update CI files

--- ci-test.sh
+++ ci-test.sh
@@ -2,9 +2,9 @@
 set -e
 
 commands=(
-    "composer install"
+    "composer update"
     "composer normalize --dry-run --indent-size=4 --indent-style=space"
-    # "vendor/bin/php-cs-fixer fix --diff --dry-run --verbose"
+    "vendor/bin/php-cs-fixer fix --diff --dry-run --verbose"
     "vendor/bin/phpmd src xml phpmd.xml"
     "vendor/bin/phpstan analyse -vvv"
     "vendor/bin/phpunit"
